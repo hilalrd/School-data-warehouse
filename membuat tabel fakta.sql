51d485a82e4b654d100229fcdcc2b9665fc0845c
hilal.amirudin@gmail.com <hilal.amirudin@gmail.com>
2023-10-26 15:09:51 +0700

    Added New ERD for School Databses
    Added star schema method to database which contain of dimension table and fact table

--- membuat tabel fakta.sql
+++
@@ -1,9 +0,0 @@
--- Tabel Fakta Absensi Siswa
-CREATE TABLE fakta_absensi_siswa (
-    siswa_id INT REFERENCES dim_siswa(siswa_id),
-    guru_id INT REFERENCES dim_guru(guru_id),
-    mata_pelajaran_id INT REFERENCES dim_mata_pelajaran(mata_pelajaran_id),
-    status_kehadiran VARCHAR(10),
-	tanggal DATE,
-    PRIMARY KEY (siswa_id, guru_id, mata_pelajaran_id, tanggal_id)
-);
